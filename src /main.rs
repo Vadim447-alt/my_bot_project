c05f32ddceaaabc2a93814adf41b074b371db04b
Vadim447-alt <vadimfuleybg@gmail.com>
2025-07-11 10:18:26 +0200

    Update main.rs

--- src /main.rs
+++ src /main.rs
@@ -1,1 +1,39 @@
 
+
+use teloxide::prelude::*;
+use teloxide::types::{InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery};
+use teloxide::utils::command::BotCommand;
+use dotenv::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    env_logger::init();
+
+    let bot = Bot::from_env().auto_send();
+
+    teloxide::repl(bot.clone(), move |msg| {
+        let bot = bot.clone();
+        async move {
+            if let Some(text) = msg.update.text() {
+                match text {
+                    "/start" => {
+                        let keyboard = InlineKeyboardMarkup::new(vec![
+                            vec![
+                                InlineKeyboardButton::callback("💎 Купить Гемы", "buy_gems"),
+                                InlineKeyboardButton::callback("🪙 Купить Робуксы", "buy_robux"),
+                            ],
+                        ]);
+                        msg.answer("Выберите товар:").reply_markup(keyboard).await?;
+                    }
+                    _ => {
+                        msg.answer("Пожалуйста, выберите товар с помощью кнопок.").await?;
+                    }
+                }
+            }
+            Ok(())
+        }
+    })
+    .await;
+}
